026b825c2b988dc862cd9edf7d6cc5f11994495d
cerealkiller <cerealkiller@red-light.(none)>
2015-04-07 23:11:32 +0200

    meteor deploy base ready

--- scripts/deploy-meteor.sh
+++ scripts/deploy-meteor.sh
@@ -1,28 +1,36 @@
-mkdir -p /var/www/paesidisandalmazzo
-cd /var/www/paesidisandalmazzo
-rm -Rf ./*
-cp /home/cerealkiller/works/meteor/paesidisandalmazzo/paesidisandalmazzo.tar.gz ./paesidisandalmazzo.tar.gz
-cd /var/www/paesidisandalmazzo && tar -zxvf paesidisandalmazzo.tar.gz
-rm ./paesidisandalmazzo.tar.gz
-mv ./bundle/* .
-rm -Rf ./bundle
-mkdir forever-logs
-cd ./programs/server && npm install
+#if folder already exists, backup it before going on
+if [ -d "<<<site-folder>>>" ]; then
+	sudo mv <<<site-folder>>> <<<site-folder>>>.bak
+fi
+
+sudo mkdir -p <<<site-folder>>>
+cd <<<site-folder>>>
+sudo cp <<<home-folder>>>/deployTmp/tmp/<<<app-name>>>.tar.gz ./<<<app-name>>>.tar.gz
+cd <<<site-folder>>> && sudo tar -zxvf <<<app-name>>>.tar.gz
+sudo rm ./<<<app-name>>>.tar.gz
+sudo mv ./bundle/* .
+sudo rm -Rf ./bundle
+cd ./programs/server && sudo npm install
 cd ../..
-mv main.js paesidisandalmazzo.js
-export MONGO_URL='mongodb://paesidisandalmazzo:yEuUpWC8QEkx@144.76.103.88:27017/paesidisandalmazzo'
-export ROOT_URL='http://localhost'
-export PORT=13000
+sudo mv main.js <<<forever-process-name>>>.js
+export MONGO_URL='mongodb://<<<mongodb-user>>>:<<<mongodb-pass>>>@<<<mongodb-address>>>:<<<mongodb-port>>>/<<<mongodb-dbname>>>'
+export ROOT_URL='http://<<<domain>>>'
+export PORT=<<<port>>>
 
 #disable websockets
-if [ "$1" == "--nows" ] ; then
+if [ "$1" = "--nows" ] ; then
     export DISABLE_WEBSOCKETS=true
 fi
 
 #stop previous running process
-oldProcess=$(forever list | grep paesidisandalmazzo)
+oldProcess=$(forever list | grep <<<forever-process-name>>>)
 if [ -n "$oldProcess" ] ; then
-    forever stop paesidisandalmazzo
+    forever stop <<<forever-process-name>>>
 fi
 
-forever --uid paesidisandalmazzo --minUptime 300 --spinSeleepTime 1000 -l /var/www/paesidisandalmazzo/forever-logs/paesidisandalmazzo.log -o /var/www/paesidisandalmazzo/forever-logs/stdout.log -e /var/www/paesidisandalmazzo/forever-logs/stderr.log start paesidisandalmazzo.js
+forever --uid <<<forever-process-name>>> --minUptime 100 --spinSeleepTime 1000 -a start <<<forever-process-name>>>.js
+
+#remove tmp backup of previous version if exists
+if [ -d "<<<site-folder>>>.bak" ]; then
+	sudo rm -Rf <<<site-folder>>>.bak
+fi
